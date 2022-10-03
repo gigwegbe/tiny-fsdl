f902167b6817749e2ba5b30364829c00d7703783
David Rose <david@drose.io>
2022-10-03 15:53:17 -0700

    move notebook code to py file

---
+++ tiny_fsdl/preprocess.py
@@ -0,0 +1,124 @@
+import logging
+import os
+from pathlib import Path
+import shutil
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+LOG = logging.getLogger(__name__)
+
+###############################
+#### Set up the parameters ####
+###############################
+
+# Image directory
+INPUT_DIR = "./ml/data/raw_images/data1"
+OUTPUT_DIR = "./ml/data/processed_images/data1"
+
+# Relative locations
+y = 11
+w = 15
+h = 25
+digits_before = 4
+digits_after = 2
+x_before = 46
+x_after = 89
+
+
+#######################
+#### Preprocessing ####
+#######################
+
+
+class Preprocess:
+    def __init__(
+        self,
+        y,
+        w,
+        h,
+        # digits_before,
+        # digits_after,
+        # x_before,
+        # x_after,
+    ):
+        self.y = y
+        self.w = w
+        self.h = h
+        # self.digits_before = digits_before
+        # self.digits_after = digits_after
+        # self.x_before = x_before
+        # self.x_after = x_after
+
+    def crop(self, img, x):
+        return img[
+            self.y : self.y + self.h,
+            x : x + self.w,
+        ]
+
+    def start(
+        self,
+        input_dir,
+        output_dir,
+        digits_before,
+        digits_after,
+        x_before,
+        x_after,
+    ):
+        LOG.info("Beginning preprocessing. . .")
+        input_dir = Path(input_dir)
+        output_dir = Path(output_dir)
+
+        shutil.rmtree(output_dir, ignore_errors=True)
+        output_dir.mkdir(parents=True, exist_ok=True)
+        assert os.path.exists(input_dir), "Input directory does not exist"
+        assert os.path.exists(output_dir), "Output directory does not exist"
+
+        crop_ix = 0
+        for img_ix, fp in enumerate(tqdm(os.listdir(input_dir))):
+            LOG.info(fp)
+            raw_img = cv2.imread(str(Path(input_dir, fp)))
+            # Digits before decimal
+            for digit in range(digits_before):
+                if digit < 0 or digit == 3 or digit == 4:
+                    continue
+                x = x_before + (digit * 15)
+                new_img = self.crop(raw_img, x)
+                cv2.imwrite(
+                    filename=str(Path(OUTPUT_DIR, f"cropped_{fp}_b_{digit}.bmp")),
+                    img=new_img,
+                )
+                crop_ix += 1
+
+            # Digits after decimal
+            for digit in range(digits_after):
+                if digit >= 0:
+                    x = x_after + (digit * 15)
+                    new_img = self.crop(raw_img, x)
+                    cv2.imwrite(
+                        filename=str(Path(OUTPUT_DIR, f"cropped_{fp}_a_{digit}.bmp")),
+                        img=new_img,
+                    )
+                    crop_ix += 1
+        LOG.info(f"Finished preprocessing!")
+        LOG.info(f"Total images {img_ix} and crops {crop_ix}")
+
+
+##############
+#### Main ####
+##############
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    preprocess = Preprocess(y, w, h)
+    preprocess.start(
+        input_dir=INPUT_DIR,
+        output_dir=OUTPUT_DIR,
+        digits_before=digits_before,
+        digits_after=digits_after,
+        x_before=x_before,
+        x_after=x_after,
+    )
